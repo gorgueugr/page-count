7df18ae93469c129d046b6c7724a013a27287768
gorguexo <solerpadial@protonmail.com>
2022-02-09 23:49:53 +0100

    Innitial commit

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.hello = void 0;
-function hello(name) {
-    return "Hello ".concat(name);
-}
-exports.hello = hello;
-//# sourceMappingURL=index.js.map
