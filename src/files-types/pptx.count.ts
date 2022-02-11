f927e4e052f04989d4500f3b971c0b3434deda0e
gorguexo <solerpadial@protonmail.com>
2022-02-11 18:41:51 +0100

    Rework pptx

--- src/files-types/pptx.count.ts
+++ src/files-types/pptx.count.ts
@@ -1,23 +1,36 @@
+import { Stream } from 'stream';
 import * as unzip from 'unzipper';
 import { parseString } from 'xml2js';
 import { ICounter } from './base.count';
 import { bufferToStream, streamToString } from "./shared";
 
 export class PptxCounter implements ICounter {
+
+
+    private static async _parseXmlFile(xml: Stream) : Promise<number> {
+        const xmlStr: string = await streamToString(xml);
+
+        return new Promise<number>((resolve) => {
+            parseString(xmlStr, (err, result) => {
+                if (err) resolve(0);
+    
+                if (result['Properties']['Slides'][0]) {
+                    resolve(parseInt(result['Properties']['Slides'][0]));
+                } 
+                resolve(0);
+            })
+        })
+    }
+
+
     static count(buffer: Buffer): Promise<number> {
-        return new Promise(async (resolve, reject) => {
+        return new Promise(async (resolve) => {
             bufferToStream(buffer)
                 .pipe(unzip.Parse())
                 .on('entry', async (entry) => {
                     if (entry.path == 'docProps/app.xml') {
-                        const entryStr: string = await streamToString(entry);
-                        parseString(entryStr, (err, result) => {
-                            if (result['Properties']['Slides'][0]) {
-                                return resolve(parseInt(result['Properties']['Slides'][0]));
-                            } else {
-                                return reject(`Cannot find page count. Error: ${err}`);
-                            }
-                        })
+                        const pages = await PptxCounter._parseXmlFile(entry);
+                        resolve(pages);
                     }
                     entry.autodrain();
                 })
