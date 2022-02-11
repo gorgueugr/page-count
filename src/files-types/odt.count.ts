9c24c13767f245987dff9ab5e08c87a3242b65c1
gorguexo <solerpadial@protonmail.com>
2022-02-11 19:15:59 +0100

    Fix odt

--- src/files-types/odt.count.ts
+++ src/files-types/odt.count.ts
@@ -4,22 +4,21 @@ import { bufferToStream, streamToString } from "./shared";
 
 export class OdtCounter implements ICounter {
     static count(buffer: Buffer): Promise<number> {
-        throw new Error('Not implemented')
-    //     return new Promise(async (resolve, reject) => {
-    //         bufferToStream(buffer)
-    //             .pipe(unzip.Parse())
-    //             .on('entry', async (entry) => {
-    //                 if (entry.path == 'meta.xml') {
-    //                     const entryStr: string = await streamToString(entry);
-    //                     const searchTerm = 'page-count="';
-    //                     const start = entryStr.indexOf(searchTerm);
-    //                     const firstStr = entryStr.substring(start + searchTerm.length, entryStr.length);
-    //                     const end = firstStr.indexOf('"');
-    //                     const numStr = firstStr.substring(0, end);
-    //                     return resolve(parseInt(numStr));
-    //                 }
-    //                 entry.autodrain();
-    //             })
-    //     })
+        return new Promise(async (resolve, reject) => {
+            bufferToStream(buffer)
+                .pipe(unzip.Parse())
+                .on('entry', async (entry) => {
+                    if (entry.path == 'meta.xml') {
+                        const entryStr: string = await streamToString(entry);
+                        const searchTerm = 'page-count="';
+                        const start = entryStr.indexOf(searchTerm);
+                        const firstStr = entryStr.substring(start + searchTerm.length, entryStr.length);
+                        const end = firstStr.indexOf('"');
+                        const numStr = firstStr.substring(0, end);
+                        return resolve(parseInt(numStr));
+                    }
+                    entry.autodrain();
+                })
+        })
     }
 }
